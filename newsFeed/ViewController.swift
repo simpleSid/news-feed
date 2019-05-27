b7a42230e0762d43e0cfe0893695aee27157be69
Денис Сидоренко <itklass@mail.ru>
2019-05-27 22:07:57 +0300

    initial commit

---
+++ newsFeed/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsFeed
+//
+//  Created by Денис Сидоренко on 27/05/2019.
+//  Copyright © 2019 Денис Сидоренко. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
